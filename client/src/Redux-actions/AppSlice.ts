9a371cef31f14c9a6338dbdf079a3015ddda2e5c
Isaac Williams <Isaaciyaye174@gmail.com>
2023-02-24 18:26:07 +0100

    New Update

--- client/src/Redux-actions/AppSlice.ts
+++ client/src/Redux-actions/AppSlice.ts
@@ -1,128 +1,136 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { NavData , TechStack} from "../data/AppData";
+import { NavData, TechStack } from "../data/AppData";
 
 // Getting Last used theme from local storage
 if (typeof window !== "undefined") {
-	// i used var because at the time i felt like i had no choice but do declare my variable globally
-	var item =
-		localStorage.getItem("currentTheme") !== null
-			? JSON.parse(`${localStorage.getItem("currentTheme")}`)
-			: [];
+  // i used var because at the time i felt like i had no choice but do declare my variable globally
+  var item =
+    localStorage.getItem("currentTheme") !== null
+      ? JSON.parse(`${localStorage.getItem("currentTheme")}`)
+      : [];
 }
 
 export interface CounterState {
-	navToggle: boolean;
-	themeToggle: boolean;
-	navData: any[];
-	currentTheme: string;
-	isChecked: boolean;
-	skillshead: any[],
-	stackhead: any[]
+  navToggle: boolean;
+  themeToggle: boolean;
+  navData: any[];
+  currentTheme: string;
+  isChecked: boolean;
+  skillshead: any[];
+  stackhead: any[];
+  formData: object;
 }
 
 const initialState: CounterState = {
-	navToggle: false,
-	themeToggle: false,
-	navData: NavData,
-	currentTheme: item,
-	isChecked: false,
-	skillshead: TechStack,
-	stackhead: []
+  navToggle: false,
+  themeToggle: false,
+  navData: NavData,
+  currentTheme: item,
+  isChecked: false,
+  skillshead: TechStack,
+  stackhead: [],
+  formData: {
+    name: "",
+    email: "",
+    message: "",
+  },
 };
 
 const appSlice = createSlice({
-	name: "app",
-	initialState,
-	reducers: {
-		setNavToggle: (state) => {
-			state.navToggle = !state.navToggle;
-			console.log(state.navToggle);
-		},
-		setThemeToggle: (state) => {
-			state.themeToggle = !state.themeToggle;
+  name: "app",
+  initialState,
+  reducers: {
+    setNavToggle: (state) => {
+      state.navToggle = !state.navToggle;
+      console.log(state.navToggle);
+    },
+    setThemeToggle: (state) => {
+      state.themeToggle = !state.themeToggle;
 
-			console.log(state.themeToggle);
-		},
-		setIsChecked: (state) => {
-			state.themeToggle = true;
+      console.log(state.themeToggle);
+    },
+    setIsChecked: (state) => {
+      state.themeToggle = true;
 
-			console.log(state.themeToggle);
-		},
-		setCloseThemeBar: (state) => {
-			state.themeToggle = false;
+      console.log(state.themeToggle);
+    },
+    setCloseThemeBar: (state) => {
+      state.themeToggle = false;
 
-			console.log(state.themeToggle);
-		},
-		setNavSelect: (state, { payload }) => {
-			// const navItem = state.navData.find((item) => item.id === payload);
-			// navItem.state = navItem ? !navItem.state : false;
+      console.log(state.themeToggle);
+    },
+    setNavSelect: (state, { payload }) => {
+      // const navItem = state.navData.find((item) => item.id === payload);
+      // navItem.state = navItem ? !navItem.state : false;
 
-			const X = state.navData.map((item) => {
-				console.log(item.id, "green");
-				return item.id === payload
-					? { ...item, state: true }
-					: { ...item, state: false };
-			});
-			state.navData = X;
-			console.log("greeen", state.navData, payload, X);
-		},
-		toggleSkillsBox: (state, { payload }) => {
-			// const navItem = state.navData.find((item) => item.id === payload);
-			// navItem.state = navItem ? !navItem.state : false;
+      const X = state.navData.map((item) => {
+        console.log(item.id, "green");
+        return item.id === payload
+          ? { ...item, state: true }
+          : { ...item, state: false };
+      });
+      state.navData = X;
+      console.log("greeen", state.navData, payload, X);
+    },
+    toggleSkillsBox: (state, { payload }) => {
+      // const navItem = state.navData.find((item) => item.id === payload);
+      // navItem.state = navItem ? !navItem.state : false;
 
-			const X = state.skillshead.map((item) => {
-				console.log(item.id, "green");
-				return item.id === payload
-					? { ...item, state: !item.state }
-					: item;
-			});
-			state.skillshead = X;
-			console.log("greeen", state.navData, payload, X);
-		},
-		setTheme: (state, { payload }) => {
-			const Toggle = document.body;
-			switch (payload) {
-				case "dim":
-					Toggle.classList.add("dim");
-					Toggle.classList.remove("black");
-					Toggle.classList.remove("coffee");
-					break;
-				case "black":
-					Toggle.classList.add("black");
-					Toggle.classList.remove("dim");
-					Toggle.classList.remove("coffee");
-					break;
-				case "coffee":
-					Toggle.classList.add("coffee");
-					Toggle.classList.remove("dim");
-					Toggle.classList.remove("black");
-					break;
-				default:
-					Toggle.classList.remove("dim");
-					Toggle.classList.remove("black");
-					Toggle.classList.remove("coffee");
-					break;
-			}
-			// Saving last clicked theme to localstorage
-			if (typeof window !== "undefined") {
-				localStorage.setItem(
-					"currentTheme",
-					JSON.stringify((state.currentTheme = payload))
-				);
-			}
-		},
-	},
+      const X = state.skillshead.map((item) => {
+        console.log(item.id, "green");
+        return item.id === payload ? { ...item, state: !item.state } : item;
+      });
+      state.skillshead = X;
+      console.log("greeen", state.navData, payload, X);
+    },
+    setTheme: (state, { payload }) => {
+      const Toggle = document.body;
+      switch (payload) {
+        case "dim":
+          Toggle.classList.add("dim");
+          Toggle.classList.remove("black");
+          Toggle.classList.remove("coffee");
+          break;
+        case "black":
+          Toggle.classList.add("black");
+          Toggle.classList.remove("dim");
+          Toggle.classList.remove("coffee");
+          break;
+        case "coffee":
+          Toggle.classList.add("coffee");
+          Toggle.classList.remove("dim");
+          Toggle.classList.remove("black");
+          break;
+        default:
+          Toggle.classList.remove("dim");
+          Toggle.classList.remove("black");
+          Toggle.classList.remove("coffee");
+          break;
+      }
+      // Saving last clicked theme to localstorage
+      if (typeof window !== "undefined") {
+        localStorage.setItem(
+          "currentTheme",
+          JSON.stringify((state.currentTheme = payload))
+        );
+      }
+    },
+    setFormData: (state, { payload }) => {
+      state.formData = {};
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
 export const {
-	setNavToggle,
-	setThemeToggle,
-	setNavSelect,
-	setCloseThemeBar,
-	setTheme,
-	setIsChecked,
-	toggleSkillsBox
+  setNavToggle,
+  setThemeToggle,
+  setNavSelect,
+  setCloseThemeBar,
+  setTheme,
+  setIsChecked,
+  toggleSkillsBox,
+  setFormData,
 } = appSlice.actions;
 
 export default appSlice.reducer;
