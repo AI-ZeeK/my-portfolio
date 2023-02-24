adf9a878e7ba0b4a319bbaa689307d23460e92af
Isaac Williams <Isaaciyaye174@gmail.com>
2023-02-24 20:18:58 +0100

    New Comit

--- client/src/pages/api/contact.ts
+++ client/src/pages/api/contact.ts
@@ -1,11 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { mailOptions, transporter } from "@/config/nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  message: string;
+  message?: string;
+  success?: boolean;
 };
 
-export default function handler(
+const contactMessageFields = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -13,6 +21,18 @@ export default function handler(
     const formData = req.body;
     if (!formData.name || !formData.email || !formData.message) {
       return res.status(400).json({ message: "Bad Request" });
+    }
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        subject: "My Website Message",
+        text: `${formData.message}`,
+        html: `<h1>${formData.name}</h1> <h2>${formData.email}</h2><p>${formData.message}</p> `,
+      });
+      return res.status(400).json({ success: true });
+    } catch (error: any) {
+      console.log(error);
+      return res.status(400).json({ message: error.message });
     }
   }
   console.log(req.body);
