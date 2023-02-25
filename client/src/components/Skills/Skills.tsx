0510e6526bbcfe231389022ee505bf73d398877d
Isaac Williams <Isaaciyaye174@gmail.com>
2023-02-25 12:09:02 +0100

    New Update

--- client/src/components/Skills/Skills.tsx
+++ client/src/components/Skills/Skills.tsx
@@ -3,51 +3,69 @@ import { TechStack } from "@/data/AppData";
 import skillsStyle from "../../styles/Skills.module.scss";
 import { motion } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
-import {toggleSkillsBox} from "../../Redux-actions/AppSlice";
+import { toggleSkillsBox } from "../../Redux-actions/AppSlice";
+import { fadeIn } from "@/utils/motion";
 
 const Skills = () => {
-	const { skillshead } =	useSelector((store: any) => store.app);
-	const dispatch = useDispatch();
-	const handleToggleSkills  = (selectId: string) => {
-		dispatch(toggleSkillsBox(selectId))
-	}
-	return (
-		<div id="skills" className={`${skillsStyle.skills_section}`}>
-			<motion.div className={`${skillsStyle.skills_head_box}`}>
-				<h1 className={`${skillsStyle.skills_head}`}>Skills</h1>
-				<p className={`${skillsStyle.skills_desc}`}>My technical level</p>
-			</motion.div>
-			<div className={`${skillsStyle.skills_card_block}`}>
-				{skillshead?.map((item: any) => (
-					<div key={item.id} className={`${skillsStyle.skills_card_box} ${item.state ? skillsStyle.active : ''} `}>
-						<div onClick={() => handleToggleSkills(item.id)} className={`${skillsStyle.skills_card_head}`}>
-							<div className={`${skillsStyle.skills_head_icon}`}>
-								{item?.icon3}
-							</div>
+  const { skillshead } = useSelector((store: any) => store.app);
+  const dispatch = useDispatch();
+  const handleToggleSkills = (selectId: string) => {
+    dispatch(toggleSkillsBox(selectId));
+  };
+  return (
+    <div id="skills" className={`${skillsStyle.skills_section}`}>
+      <motion.div className={`${skillsStyle.skills_head_box}`}>
+        <h1 className={`${skillsStyle.skills_head}`}>Skills</h1>
+        <p className={`${skillsStyle.skills_desc}`}>My technical level</p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("up", "tween", 0, 1)}
+        whileInView="show"
+        initial="hidden"
+        viewport={{ once: false, amount: 0.2 }}
+        className={`${skillsStyle.skills_card_block}`}>
+        {skillshead?.map((item: any) => (
+          <div
+            key={item.id}
+            className={`${skillsStyle.skills_card_box} ${
+              item.state ? skillsStyle.active : ""
+            } `}>
+            <div
+              onClick={() => handleToggleSkills(item.id)}
+              className={`${skillsStyle.skills_card_head}`}>
+              <div className={`${skillsStyle.skills_head_icon}`}>
+                {item?.icon3}
+              </div>
 
-							<h1 className={`${skillsStyle.skills_head_desc}`}>{item.desc}</h1>
-							<div className={`${skillsStyle.skills_head_chev}`}>
-								{item.state ? item?.icon1 : item?.icon2 }
-							</div>
-							{/* <div>{}</div> */}
-						</div>
-						<div className={`${skillsStyle.stack_card_block}`}>
-							{item.stack.map((stack : any) => (
-								<div className={`${skillsStyle.stack_card_box}`} key={stack.id}>
-									<div className={`${skillsStyle.stack_card_image}`}>
-										<Image className={`${skillsStyle.img}`} width={25} height={25} src={stack.img} alt="alt" />
-									</div>
-									<h1 className={`${skillsStyle.stack_card_desc}`}>
-										{stack.desc}
-									</h1>
-								</div>
-							))}
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+              <h1 className={`${skillsStyle.skills_head_desc}`}>{item.desc}</h1>
+              <div className={`${skillsStyle.skills_head_chev}`}>
+                {item.state ? item?.icon1 : item?.icon2}
+              </div>
+              {/* <div>{}</div> */}
+            </div>
+            <div className={`${skillsStyle.stack_card_block}`}>
+              {item.stack.map((stack: any) => (
+                <div className={`${skillsStyle.stack_card_box}`} key={stack.id}>
+                  <div className={`${skillsStyle.stack_card_image}`}>
+                    <Image
+                      className={`${skillsStyle.img}`}
+                      width={25}
+                      height={25}
+                      src={stack.img}
+                      alt="alt"
+                    />
+                  </div>
+                  <h1 className={`${skillsStyle.stack_card_desc}`}>
+                    {stack.desc}
+                  </h1>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Skills;
--- client/src/components/about/About.tsx
+++ client/src/components/about/About.tsx
@@ -5,47 +5,57 @@ import aboutStyle from "../../styles/About.module.scss";
 
 import { motion } from "framer-motion";
 import { HiOutlineDownload } from "react-icons/hi";
+import { fadeIn } from "@/utils/motion";
 const aboutImage = "/avel.jpg";
 const About = () => {
-	return (
-		<div id="about" className={`${aboutStyle.about_section}`}>
-			<motion.div className={`${aboutStyle.about_head_box}`}>
-				<h1 className={`${aboutStyle.about_head}`}>About</h1>
-				<p className={`${aboutStyle.about_desc}`}>My Introduction</p>
-			</motion.div>
-			<motion.div className={`${aboutStyle.about_body_block}`}>
-				<div className={`${aboutStyle.about_image}`}>
-					<Image
-						className={`${aboutStyle.img}`}
-						width={200}
-						height={200}
-						alt="alternate"
-						src={aboutImage}
-					/>
-				</div>
-				<div  className={`${aboutStyle.about_info_block}`}>
-					<div className={`${aboutStyle.about_info_box}`}>
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Doloremque dolor voluptate ea dolorem aliquid modi placeat
-							deleniti repudiandae praesentium mollitia.{" "}
-						</p>
-					</div>
-					<div className={`${aboutStyle.about_info_box}`}>
-						<strong>1+</strong>
-						<small>years <br />
-						Experience</small>
-					</div>
-					<div className={`${aboutStyle.about_info_box}`}>
-						<Link href="" download="" className={`${aboutStyle.btn_primary}`}>
-							<span>Download CV</span>
-							<HiOutlineDownload 	className={`text-[1.4rem] transition ${aboutStyle.icon}`} /> 
-						</Link>
-					</div>
-				</div>
-			</motion.div>
-		</div>
-	);
+  return (
+    <div id="about" className={`${aboutStyle.about_section}`}>
+      <motion.div className={`${aboutStyle.about_head_box}`}>
+        <h1 className={`${aboutStyle.about_head}`}>About</h1>
+        <p className={`${aboutStyle.about_desc}`}>My Introduction</p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("left", "tween", 0, 1)}
+        whileInView="show"
+        initial="hidden"
+        viewport={{ once: false, amount: 0.05 }}
+        className={`${aboutStyle.about_body_block}`}>
+        <div className={`${aboutStyle.about_image}`}>
+          <Image
+            className={`${aboutStyle.img}`}
+            width={200}
+            height={200}
+            alt="alternate"
+            src={aboutImage}
+          />
+        </div>
+        <div className={`${aboutStyle.about_info_block}`}>
+          <div className={`${aboutStyle.about_info_box}`}>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloremque dolor voluptate ea dolorem aliquid modi placeat
+              deleniti repudiandae praesentium mollitia.{" "}
+            </p>
+          </div>
+          <div className={`${aboutStyle.about_info_box}`}>
+            <strong>1+</strong>
+            <small>
+              years <br />
+              Experience
+            </small>
+          </div>
+          <div className={`${aboutStyle.about_info_box}`}>
+            <Link href="" download="" className={`${aboutStyle.btn_primary}`}>
+              <span>Download CV</span>
+              <HiOutlineDownload
+                className={`text-[1.4rem] transition ${aboutStyle.icon}`}
+              />
+            </Link>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default About;
