d52a2fb7a3ae1411b8fc43bd6e6e304bd4ce18c4
Isaac Williams <Isaaciyaye174@gmail.com>
2023-03-18 20:09:28 +0100

    Update About.tsx

--- client/src/components/about/About.tsx
+++ client/src/components/about/About.tsx
@@ -50,7 +50,7 @@ const About = () => {
 					</div> */}
 					<div className={`${aboutStyle.about_info_box}`}>
 						<Link
-							href=""
+							href="https://drive.google.com/file/d/1U67MM0Ts0aRkjLFCXDNLoSnZJkbf4ZY2/view?usp=drivesdk"
 							download=""
 							className={`${aboutStyle.btn_primary}`}>
 							<span>Download CV</span>
